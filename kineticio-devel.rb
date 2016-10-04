379459fb80ac49aea33a8a5f681c6270c6797d83
Elvin Sindrilaru <esindril@cern.ch>
2016-10-04 17:00:30 +0200

    Update to version v1.3

--- kineticio-devel.rb
+++ kineticio-devel.rb
@@ -1,8 +1,8 @@
 class KineticioDevel < Formula
   desc "Public Header Files of the Kineticio Library."
   homepage "https://github.com/cern-eos/kineticio"
-  url "http://dss-ci-repo.web.cern.ch/dss-ci-repo/kinetic/kineticio/noarch/kineticio-1.2-devel.tar.gz"
-  sha256 "672a38e7c29261030363b54688fdfbfbb71a971ec563188d34522017c2bef66b"
+  url "http://dss-ci-repo.web.cern.ch/dss-ci-repo/kinetic/kineticio/noarch/kineticio-1.3-devel.tar.gz"
+  sha256 "2767a395b49b7049ef99eed557f9f330770b0cb8da293a3efea7c75f12cda573"
 
   def install
     include.mkpath
